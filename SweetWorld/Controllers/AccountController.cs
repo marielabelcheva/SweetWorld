497bfaf7f2a3653fb062789e611d2fa822525275
marielabelcheva <meribelcheva@gmail.com>
2024-02-21 10:57:25 +0200

    Added login and register

---
+++ SweetWorld/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SweetWorld.Data.Models;
+using SweetWorld.Models.AccountViewModels;
+
+namespace SweetWorld.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false) { return RedirectToAction("Index", "Home"); }
+
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) { return View(model); }
+
+            User user = new User()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                UserName = model.UserName
+            };
+
+            var result = await this.userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded) { return RedirectToAction("Login", "Account"); }
+
+            foreach (var error in result.Errors) { ModelState.AddModelError("", error.Description); }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false) { return RedirectToAction("Index", "Home"); }
+
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) { return View(model); }
+
+            User user = await this.userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await this.signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded) { return RedirectToAction("Index", "Home"); }
+            }
+
+            ModelState.AddModelError("", "Invalid login!");
+            return View(model);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SweetWorld/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SweetWorld.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string? UserName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(60, MinimumLength = 10)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+    }
+}
