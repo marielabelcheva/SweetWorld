7fa8578e795912eaf5f253ab83214a370cdae434
marielabelcheva <meribelcheva@gmail.com>
2024-03-24 23:04:36 +0200

    Added services

--- SweetWorld/Program.cs
+++ SweetWorld/Program.cs
@@ -22,8 +22,13 @@ builder.Services.AddControllersWithViews();
 builder.Services.ConfigureApplicationCookie(options => options.LoginPath = "/User/Login");
 
 builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<IIngredientService, IngredientService>();
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IConfectionerService, ConfectionerService>();
 builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var cloudinarySettings = builder.Configuration.GetSection("CloudinarySettings").Get<CloudinarySettings>();
 builder.Services.AddSingleton<Cloudinary>((sp) =>
